ee8aebbbab04da124c541916902c6ed3e6582125
Lukas <82582433+Serpant4632@users.noreply.github.com>
2022-05-25 17:21:16 +0200

    added python scripts

---
+++ Linux_CPU/main.py
@@ -0,0 +1,20 @@
+import psutil
+import socekt
+import time
+
+TARGET_IP = "192.168.0.193"
+TARGET_PORT = 1234
+
+while True:
+    temperature = 0.0
+    for x in rang(5):
+        temperature += psutil.sensors_temperatures()["coretemp"][0].current
+        time.sleep(0.5)
+    temperature /= 5.0
+
+    cores = psutil.cpu_percent(percpu=True)
+
+    out = str(temperature) + ',' + ','.join(map(str, sorted(cores, reverse=True)))
+    print(out)
+    socket.socket(socket.AF_INET, socket.SOCK_DGRAM).sendto(out.encode('utf-8'), (TARGET_IP,TARGET_PORT))
+    
